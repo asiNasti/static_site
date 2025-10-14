e82355e96c1540a7547cd9b4339ed22e22d42860
asi_nasti <fohelanastasiia@gmail.com>
2025-10-14 14:17:59 +0000

    create full text to node converter

--- src/test_processing.py
+++ src/test_processing.py
@@ -1,6 +1,9 @@
 import unittest
 
-from text_processing import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from text_processing import text_node_to_html_node, split_nodes_delimiter
+from text_processing import extract_markdown_images, extract_markdown_links
+from text_processing import split_nodes_image, split_nodes_link, text_to_textnodes
+
 from textnode import TextType, TextNode
 
 class TestNodeToHtml(unittest.TestCase):
@@ -106,7 +109,8 @@ class TestExtractMarkdown(unittest.TestCase):
 class TestSplitNodesImageLinks(unittest.TestCase):
     def test_split_images(self):
         node = TextNode(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) "
+            "and another ![second image](https://i.imgur.com/3elNhQu.png)",
             TextType.TEXT,)
         
         actual_nodes = split_nodes_image([node])
@@ -119,7 +123,8 @@ class TestSplitNodesImageLinks(unittest.TestCase):
 
     def test_split_links(self):
         node = TextNode(
-            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            "This is text with a link [to boot dev](https://www.boot.dev) "
+            "and [to youtube](https://www.youtube.com/@bootdotdev)",
             TextType.TEXT,)
         
         actual_nodes = split_nodes_link([node])
@@ -129,6 +134,25 @@ class TestSplitNodesImageLinks(unittest.TestCase):
                           TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")]
         
         self.assertListEqual(actual_nodes, expected_nodes)
+
+class TestTextToNodes(unittest.TestCase):
+    def test_full(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+        actual_nodes = text_to_textnodes(text)
+        expected_nodes = [TextNode("This is ", TextType.TEXT),
+                          TextNode("text", TextType.BOLD),
+                          TextNode(" with an ", TextType.TEXT),
+                          TextNode("italic", TextType.ITALIC),
+                          TextNode(" word and a ", TextType.TEXT),
+                          TextNode("code block", TextType.CODE),
+                          TextNode(" and an ", TextType.TEXT),
+                          TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                          TextNode(" and a ", TextType.TEXT),
+                          TextNode("link", TextType.LINK, "https://boot.dev")]
+        
+        self.assertListEqual(actual_nodes, expected_nodes)
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/text_processing.py
+++ src/text_processing.py
@@ -120,3 +120,18 @@ def split_nodes_link(old_nodes):
             if current_text != "":
                 new_nodes.append(TextNode(current_text, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    node = split_nodes_image([node])
+    node = split_nodes_link(node)
+
+    delimiters = ["**",  "_", "`"]
+    text_type = [TextType.BOLD, TextType.ITALIC, TextType.CODE]
+
+    for i in range(len(delimiters)):
+        node = split_nodes_delimiter(node, delimiters[i], text_type[i])
+    return node
+
+text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+print(text_to_textnodes(text))
